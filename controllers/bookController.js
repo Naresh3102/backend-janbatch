b938b0d1d826bae8e005f3503c2930a4c1b0f0cb
Naresh3102 <naresh31122002@gmail.com>
2026-01-07 15:30:08 +0530

    Pagination and filter

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -24,11 +24,43 @@ const createBook = async (req, res) => {
 
 const getAllBooks = async (req, res) => {
   try {
-    const books = await Book.find();
+    console.log(req.query);
+    console.log(req.query.category.split(","));
+
+    const pageNumber = parseInt(req.query.page || 1);
+    const pageSize = parseInt(req.query.size || 5);
+    const skipNumber = (pageNumber - 1) * pageSize;
+
+    const filter = {};
+
+    if (req.query.minPage || req.query.maxPage) {
+      filter.pages = {};
+      if (req.query.minPage) filter.pages.$gte = parseInt(req.query.minPage);
+      if (req.query.maxPage) filter.pages.$lte = parseInt(req.query.maxPage);
+    }
+
+    if (req.query.minPrice || req.query.maxPrice) {
+      filter.price = {};
+      if (req.query.minPrice) filter.price.$gte = parseInt(req.query.minPrice);
+      if (req.query.maxPrice) filter.price.$lte = parseInt(req.query.maxPrice);
+    }
+
+    if (req.query.author) {
+      filter.author = req.query.author;
+    }
+
+    const books = await Book.find(filter).skip(skipNumber).limit(pageSize);
+
+    const totalBooks = await Book.countDocuments();
+    const totalPages = Math.ceil(totalBooks / pageSize);
 
     res.json({
       success: true,
       books,
+      currentPage: pageNumber,
+      pageSize,
+      total: totalBooks,
+      totalPages,
     });
   } catch (error) {
     res.status(500).json({
